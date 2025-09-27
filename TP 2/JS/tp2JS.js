a677c6118fe6f8d0c28c44c0ea5426a8eeb7045d
Magui <magaliagustinacejas@gmail.com>
2025-09-27 14:42:10 -0300

    menu usuario-nuevo tipo letra- + componentesPag

--- TP 2/JS/tp2JS.js
+++ TP 2/JS/tp2JS.js
@@ -3,8 +3,18 @@
 let btnOpen = document.querySelector('#btn-open');
 btnOpen.addEventListener('click', toggleMenu);
 
+
+
 /* Abre o cierra el menú responsive*/
 function toggleMenu() {
     let menu = document.querySelector('.main-nav');
     menu.classList.toggle('open');
+}
+
+let btnMenuUsuario= document.querySelector('#btn-usuario')
+btnMenuUsuario.addEventListener('click', toggleMenu2)
+
+function toggleMenu2() {
+    let menu = document.querySelector('.main-nav-user');
+    menu.classList.toggle('openUser');
 }
