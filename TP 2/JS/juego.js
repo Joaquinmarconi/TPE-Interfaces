7b6ed57fd134c39af93fc266c40ae6fcc0262ee6
Magui <magaliagustinacejas@gmail.com>
2025-10-05 12:04:30 -0300

    ocultar popover y que se cierre al hacer click en cualquier sitio

--- TP 2/JS/juego.js
+++ TP 2/JS/juego.js
@@ -66,10 +66,21 @@ for (let i = 1; i <= 5; i++) {
   })};
 
 
-  const btn = document.getElementById('btn-suscribirse');
-  const popover = document.getElementById('mensaje-subscripcion');
+  const btnSus = document.getElementById('btn-suscribirse');
 
     // al hacer click, mostramos o cerramos el popover
-    btn.addEventListener('click', () => {
-      popover.togglePopover(); // alterna entre abierto/cerrado
-    });
+    btnSus.addEventListener('click', togglePopoverSus)
+
+    function togglePopoverSus() {
+    let popsus = document.querySelector('.popover-suscripcion');
+    popsus.classList.toggle('open');
+}
+
+let popsus = document.querySelector('.popover-suscripcion');
+// cerrar popover al hacer click fuera de él
+window.addEventListener('click', function(e) {
+  // si el click NO es dentro del popover y NO es en el botón
+  if (!popsus.contains(e.target) && e.target !== btnSus) {
+    popsus.classList.remove('open');
+  }
+});
